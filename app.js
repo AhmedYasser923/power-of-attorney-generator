0a1d8ec024d43ce63e39cb359dd477b074cbe693
AhmedYasser923 <134349923+AhmedYasser923@users.noreply.github.com>
2026-03-27 01:31:37 +0200

    .

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ dns.setServers(['8.8.8.8', '8.8.4.4']);
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const express = require('express');
-
 const bodyParser = require('body-parser');
 const path = require('path');
-
 const app = express();
+
 
 
 
@@ -23,6 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Routes
 app.use('/', require('./routes/index'));
 
+
+
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
@@ -37,7 +38,10 @@ app.use((req, res) => {
   res.status(404).render('error', { message: 'Page not found' });
 });
 
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://127.0.0.1:${PORT}`);
 });
+
